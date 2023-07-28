a372ab6a603b5226772860e54747ead3d2d54da4
John <giannis.fotopoulos@gmail.com>
2023-07-28 14:13:33 +0300

    final changes of v2.0

--- DotaFetch.py
+++ DotaFetch.py
@@ -4,16 +4,16 @@ import requests
 conn = sqlite3.connect('../dota.sqlite')
 cur = conn.cursor()
 
-call1 = 'https://api.opendota.com/api/heroes'
+heroes_call = 'https://api.opendota.com/api/heroes'
 
 # get hero id - name correlation from the API
 try:
-    response = requests.get(call1)
+    response = requests.get(heroes_call)
     response.raise_for_status()
     heroes = response.json()
 
     cur.execute(
-        "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='Heronames'")  # count : how many rows in sqlite_master table match the condition
+        "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='Heronames'")
     table_exists = cur.fetchone()[0]  # if table_exists: is true for table_exists /= 0 and false for table_exists = 0
     if table_exists:
         print('Table \'Heronames\' already exists. Insert statements will not be executed.')
@@ -30,18 +30,18 @@ try:
                 VALUES (?, ?)''', (hero_id, hero_name))
         conn.commit()
         print("Data inserted successfully in 'Heronames' table!")
-except requests.exceptions.RequestException as e:
-    print("An error occurred while making the HTTP request:", e)
-except sqlite3.Error as e:
-    print("An error occurred while interacting with the database:", e)
+except requests.exceptions.RequestException as sql_error:
+    print("An error occurred while making the HTTP request:", sql_error)
+except sqlite3.Error as sql_error:
+    print("An error occurred while interacting with the database:", sql_error)
 
-print("Moving on to call2 for retrieving all turbo match IDs")
+print("Moving on to all matches call for retrieving all turbo match IDs")
 
-# you can change the limit argument in the call bellow to make it faster. with 50 games it will take about 30 seconds to run
-call2 = 'https://api.opendota.com/api/players/197497174/matches?limit=30&game_mode=23&significant=0'
+
+all_matches = 'https://api.opendota.com/api/players/197497174/matches?limit=30&game_mode=23&significant=0'
 
 try:
-    response = requests.get(call2)
+    response = requests.get(all_matches)
     response.raise_for_status()
     matches = response.json()
     cur.execute('''CREATE TABLE IF NOT EXISTS Matches (
@@ -59,12 +59,12 @@ try:
         VALUES (?, ?)''', (match_id, game_mode))
     conn.commit()
     print("Data inserted (or ignored :) successfully in 'Matches' table!")
-except requests.exceptions.RequestException as e:
-    print("An error occurred while making the HTTP request:", e)
-except sqlite3.Error as e:
-    print("An error occurred while interacting with the database:", e)
+except requests.exceptions.RequestException as sql_error:
+    print("An error occurred while making the HTTP request:", sql_error)
+except sqlite3.Error as sql_error:
+    print("An error occurred while interacting with the database:", sql_error)
 
-print("Moving on to call3 for retrieving individual match details from Match IDs")
+print("Moving on to individual matches call for retrieving individual match details from Match IDs")
 
 baseurl = 'https://api.opendota.com/api/matches/'
 
@@ -75,11 +75,11 @@ cur.execute("SELECT Match_id FROM Matches ORDER BY Match_id DESC")
 matchids = cur.fetchall()
 
 hero_id = None  # we used this variable in the first table
-e = None        # we use e as success  flag
+sql_error = None        # success flag
 for matchid in matchids:
-    call3 = baseurl + str(matchid[0])
+    indiv_matches_cal = baseurl + str(matchid[0])
     try:
-        response = requests.get(call3)
+        response = requests.get(indiv_matches_cal)
         response.raise_for_status()
         details = response.json()
 
@@ -109,25 +109,22 @@ for matchid in matchids:
             Player_slot_9 INTEGER, 
             Player_slot_10 INTEGER          
         )''')
-        for key in details:
-            radiant_score = details['radiant_score']
-            dire_score = details['dire_score']
-            radiant_win = details['radiant_win']
-            players_data = details['players']
-            accounts = []
-            heroes = []
-            for player in players_data:
-                account_id = player['account_id']
-                hero_id = player['hero_id']
-                player_slot = player['player_slot']
-                accounts.append(account_id)
-                heroes.append(hero_id)
-
+        radiant_score = details['radiant_score']
+        dire_score = details['dire_score']
+        radiant_win = details['radiant_win']
+        players_data = details['players']
+        accounts = []
+        heroes = []
+        for player in players_data:
+            account_id = player['account_id']
+            hero_id = player['hero_id']
+            player_slot = player['player_slot']
+            accounts.append(account_id)
+            heroes.append(hero_id)
 
         cur.execute('''UPDATE Matches 
                     SET Radiant_score = ?, Dire_score = ?, Radiant_win = ?
                     WHERE Match_id = ?''', (radiant_score, dire_score, radiant_win, matchid[0]))
-
 
         cur.execute('''INSERT OR IGNORE INTO Picks (Match_id, Pick_slot_1, Pick_slot_2, Pick_slot_3, Pick_slot_4,
             Pick_slot_5, Pick_slot_6, Pick_slot_7, Pick_slot_8, Pick_slot_9, Pick_slot_10)
@@ -143,11 +140,11 @@ for matchid in matchids:
 
         conn.commit()
 
-    except requests.exceptions.RequestException as e:
-        print("An error occurred while making the HTTP request:", e)
-    except sqlite3.Error as e:
-        print("An error occurred while interacting with the database:", e)
-if e is None:
+    except requests.exceptions.RequestException as sql_error:
+        print("An error occurred while making the HTTP request:", sql_error)
+    except sqlite3.Error as sql_error:
+        print("An error occurred while interacting with the database:", sql_error)
+if sql_error is None:
     print("Data inserted (or ignored :) successfully in 'Picks' table!")
     print("Data inserted (or ignored :) successfully in 'Players' table!")
 
@@ -156,16 +153,17 @@ cur.close()
 conn.close()
 
 # CHANGES:
-# 1) changed table names (for eg Match_IDs to Matches, Game_Details to Picks, Player_details to Players)
+# 1) changed table names (for e.g. Match_IDs to Matches, Game_Details to Picks, Player_details to Players)
 # 2) removed counts from last loops
 # 3) changed columns first letters to Uppercase
 # 4) reworked database schema and removed player_slot_i columns and relevant variables
-# 5) used an Update statement (instead of insert) to fix faulty data filling in the last 3 columns of the "Matches" table
+# 5) used an Update statement (instead of insert) to fix faulty data insertion in the last 3 columns of the "Matches"
+# table
+# 6) changed some variable names to be more explanatory (like call1 , call2 , call3 and error variables)
+# 7) removed unnecessary for loop to read json 1st level dictionaries
 
 
 # THINGS TO DO:
-# 1) make CREATE and INSERT statements easier for large tables
-# 2) read about Update syntax
-# 3) write some retrieval scripts
-
-
+# 1) put sleep time to not overwhelm API
+# 2) change database model because of bad querying functionality
+# 3)
